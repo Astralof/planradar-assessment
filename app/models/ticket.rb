ec436f570150cdb7bc171375fef52a771b3b6d01
Astralof <alexander@fiedler.ink>
2022-11-18 17:58:05 +0100

    Add and test Ticket methods

--- app/models/ticket.rb
+++ app/models/ticket.rb
@@ -1,3 +1,21 @@
 class Ticket < ApplicationRecord
-  
+  belongs_to :user
+  after_create :set_user_notification
+
+  delegate :wants_notifications, to: :user
+
+  def set_user_notification
+    return unless wants_notifications
+    UserTicketNotificationJob.set(
+      wait_until: notification_datetime
+    ).perform_later(id)
+  end
+
+  def notification_datetime
+    (due_date - user.send_due_date_reminder.to_i.days + user_reminder_time_in_seconds).to_datetime
+  end
+
+  def user_reminder_time_in_seconds
+    user.due_date_reminder_time.seconds_since_midnight.seconds
+  end
 end
--- spec/models/ticket_spec.rb
+++ spec/models/ticket_spec.rb
@@ -2,4 +2,41 @@ RSpec.describe Ticket, type: :model do
   it 'has a valid factory' do
     expect(FactoryBot.create(:ticket)).to be_valid
   end
+
+  describe 'callbacks' do
+    describe 'after_create' do
+      describe '#set_user_notification' do
+        subject { FactoryBot.create(:ticket) }
+
+        it 'have enqueued job UserTicketNotificationJob' do
+          ActiveJob::Base.queue_adapter = :test
+          expect { subject }.to have_enqueued_job(UserTicketNotificationJob)
+        end
+      end
+    end
+  end
+
+  describe '#user_reminder_time_in_seconds' do
+    subject { ticket.user_reminder_time_in_seconds }
+    
+    let(:ticket) { FactoryBot.create(:ticket, user: user) }
+    let(:user) { FactoryBot.create(:user, due_date_reminder_time: time) }
+    let(:time)  { Time.now }
+
+    it 'returns the right time in seconds' do
+      expect(subject).to eq(time.seconds_since_midnight.seconds)
+    end
+  end
+
+  describe '#notification_datetime' do
+    subject { ticket.notification_datetime }
+    
+    let(:ticket) { FactoryBot.create(:ticket, user: user) }
+    let(:user) { FactoryBot.create(:user, due_date_reminder_time: time) }
+    let(:time)  { Time.now }
+
+    it 'returns the right time in seconds' do
+      expect(subject.to_s).to eq(time.to_datetime.to_s)
+    end
+  end
 end
