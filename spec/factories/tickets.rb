24ab7cd97c09167950db3dcb8e7c42e9a58efcb1
Astralof <alexander@fiedler.ink>
2022-11-18 17:54:36 +0100

    Fix factories

--- spec/factories/tickets.rb
+++ spec/factories/tickets.rb
@@ -1,6 +1,9 @@
 FactoryBot.define do
   factory :ticket do
+    association :user
+
     title { "test" }
-    description { "test" }
+    description { "Clean the floor." }
+    due_date { Date.tomorrow }
   end
 end
--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -2,5 +2,8 @@ FactoryBot.define do
   factory :user do
     name { "test" }
     mail { "test" }
+    wants_notifications { true }
+    send_due_date_reminder { 1 }
+    due_date_reminder_time { Time.now }
   end
 end
